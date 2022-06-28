ffe74228d9da8323be678f6cbbd45b1f3aa52ddb
prash <87913082+codewithprash@users.noreply.github.com>
2022-06-29 04:41:24 +0530

    3.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "15f10893ab9bbba9b0cd02842ef69210",
 "icons/Icon-maskable-192.png": "3832607aa5419feb7a66309cc07576b1",
 "icons/Icon-maskable-512.png": "c73811cbba7929752012a47b416f4ae6",
-"index.html": "1f385f4763496e459417edaf475272dc",
-"/": "1f385f4763496e459417edaf475272dc",
-"main.dart.js": "124c2f1e881a7d535c91fee6adf84a64",
+"index.html": "e252384e59cd0b1f587c8eb0409ed5df",
+"/": "e252384e59cd0b1f587c8eb0409ed5df",
+"main.dart.js": "f481331aac7cad1b32a7c6b20eef6df2",
 "manifest.json": "ed33d732d47961f6f2c2d891cb2c30ed",
 "version.json": "4751b5d763f22975e56dc574b2749261"
 };
